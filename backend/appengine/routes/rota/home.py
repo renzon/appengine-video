45264a158c556cc6d08ba02638fb4f5a6a65bd7a
renzon <renzon@gmail.com>
2014-08-15 13:40:45 -0300

    Passagem de Contexto para Template

--- backend/appengine/routes/rota/home.py
+++ backend/appengine/routes/rota/home.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
 
 
 @login_not_required
 @no_csrf
-def index(_resp):
-    _resp.write('Rota')
+def index(nome, sobrenome):
+    contexto = {'name': nome, 'lastname': sobrenome}
+    return TemplateResponse(contexto)

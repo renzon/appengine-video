e48030ca3f35149c0b047e403f16165525820ba7
renzon <renzon@gmail.com>
2014-08-15 14:57:16 -0300

    If e For

--- backend/appengine/routes/rota/home.py
+++ backend/appengine/routes/rota/home.py
@@ -7,6 +7,14 @@ from gaepermission.decorator import login_not_required
 
 @login_not_required
 @no_csrf
-def index(nome, sobrenome):
-    contexto = {'name': nome, 'lastname': sobrenome}
+def index(nome):
+    class Pessoa(object):
+        def __init__(self, nome, sobrenome):
+            self.sobrenome = sobrenome
+            self.nome = nome
+
+    pessoas = [Pessoa('Renzo', 'Nuccitelli'), Pessoa('Giovane', 'Liberato'),
+               Pessoa('Reginaldo','Silva')]
+
+    contexto = {'nome': nome,'pessoas':pessoas}
     return TemplateResponse(contexto)

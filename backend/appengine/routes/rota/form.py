a86e96d8acbd8e14cab7200a229c10045e2163c4
renzon <renzon@gmail.com>
2014-08-27 22:04:24 -0300

    Usuário Logado

--- backend/appengine/routes/rota/form.py
+++ backend/appengine/routes/rota/form.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 from config.template_middleware import TemplateResponse
 from gaecookie.decorator import no_csrf
-from gaepermission.decorator import login_not_required, permissions
+from gaepermission.decorator import login_not_required, permissions, login_required
 from permission_app.model import ADMIN, GERENTE
 from tekton import router
 
@@ -16,3 +16,10 @@ def index():
 @login_not_required
 def salvar(_resp, nome):
     _resp.write(nome)
+
+@login_required
+@no_csrf
+def usuario(_resp,_logged_user):
+    _resp.write('Usuário Logado %s'% _logged_user.key.id())
+
+

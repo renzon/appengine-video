cd35700f29088a3a14ef4585d4e46421c438fc6b
renzon <renzon@gmail.com>
2014-11-14 17:45:49 -0200

    Task defaul funcionando

--- backend/appengine/routes/vendas/tarefas.py
+++ backend/appengine/routes/vendas/tarefas.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 import logging
+from google.appengine.api import taskqueue
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
+from tekton.router import to_path
 
 
 @login_not_required
 @no_csrf
-def contagem():
-    logging.info('Tarefa de contagem')
+def contagem(total='0'):
+    # buscar um pagamento
+    # incremetar o total das vendas
+    # faço a busca novamente
+    total = int(total)
+    logging.info('Total = %s' % total)
+    total += 1
+    if total < 3:
+        taskqueue.add(url=to_path(contagem), params={'total': str(total)})

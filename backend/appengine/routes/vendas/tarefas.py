72881a2d59512200b88617df1d201d32632a54a9
renzon <renzon@gmail.com>
2014-11-18 23:23:38 -0200

    Fila de tarefa funcionando para contabilização de vendas

--- backend/appengine/routes/vendas/tarefas.py
+++ backend/appengine/routes/vendas/tarefas.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from decimal import Decimal
 import logging
 from google.appengine.api import taskqueue
 from gaebusiness.gaeutil import TaskQueueCommand
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
 from tekton.router import to_path
+from venda_app import venda_facade
 
 
 @login_not_required
 @no_csrf
-def contagem(total='0', cursor=None):
-    # buscar um pagamento
-    # incremetar o total das vendas
-    # faço a busca novamente
-    total = int(total)
-    logging.info('Total = %s' % total)
-    total += 1
-    if total < 3:
-        cmd = TaskQueueCommand('rapida', to_path(contagem), params={'total': str(total)})
+def contagem(total='0.00', cursor=None):
+    busca_cmd = venda_facade.contabilizar_venda_cmd(cursor)
+    venda = busca_cmd()
+    if venda is None:
+        logging.info(total)
+    else:
+        total = Decimal(total)
+        total += venda.preco
+        cmd = TaskQueueCommand('rapida', to_path(contagem),
+                               params={'total': str(total), 'cursor': busca_cmd.cursor.urlsafe()})
         cmd()

d56bfa8d9693354aef2129ace778accaa2714062
renzon <renzon@gmail.com>
2014-11-18 23:01:35 -0200

    Pronto comando de contabilização

--- backend/apps/venda_app/venda_commands.py
+++ backend/apps/venda_app/venda_commands.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from gaebusiness.gaeutil import SaveCommand, ModelSearchCommand
+from gaebusiness.business import CommandParallel
+from gaebusiness.gaeutil import SaveCommand, ModelSearchCommand, SingleModelSearchCommand
 from gaeforms.ndb.form import ModelForm
 from gaegraph.business_base import UpdateNode
-from venda_app.venda_model import Venda
+from venda_app.venda_model import Venda, NOVA, CONTABILIZADA
 
 
 class VendaSaveForm(ModelForm):
@@ -32,5 +33,28 @@ class UpdateVendaCommand(UpdateNode):
 class ListVendaCommand(ModelSearchCommand):
     def __init__(self):
         super(ListVendaCommand, self).__init__(Venda.query_by_creation())
+
+
+class BuscarVendaPorStatusCmd(SingleModelSearchCommand):
+    def __init__(self, status=NOVA, start_cursor=None, offset=0, use_cache=False):
+        super(BuscarVendaPorStatusCmd, self).__init__(Venda.query_by_stats_order_by_creation(status), start_cursor,
+                                                      offset, use_cache)
+
+
+class ContabilizarVendaCmd(CommandParallel):
+    def __init__(self, start_cursor=None):
+        self.cursor = None
+        cmd = BuscarVendaPorStatusCmd(NOVA, start_cursor=start_cursor)
+        self._busca_cmd = cmd
+        super(ContabilizarVendaCmd, self).__init__(cmd)
+
+    def do_business(self):
+        super(ContabilizarVendaCmd, self).do_business()
+        self.cursor = self._busca_cmd.cursor
+        venda = self.result
+        if venda:
+            venda.status = CONTABILIZADA
+            self._to_commit = venda
+
 
 

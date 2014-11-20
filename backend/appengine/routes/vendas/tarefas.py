86a45c5a2300b601ca140d85c2be791b264c8740
renzon <renzon@gmail.com>
2014-11-20 12:19:18 -0200

    Emails

--- backend/appengine/routes/vendas/tarefas.py
+++ backend/appengine/routes/vendas/tarefas.py
@@ -2,10 +2,14 @@
 from __future__ import absolute_import, unicode_literals
 from decimal import Decimal
 import logging
-from google.appengine.api import taskqueue
+
+from google.appengine.api import mail
+from config.template import render
+
 from gaebusiness.gaeutil import TaskQueueCommand
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required
+import settings
 from tekton.router import to_path
 from venda_app import venda_facade
 
@@ -15,8 +19,13 @@ from venda_app import venda_facade
 def contagem(total='0.00', cursor=None):
     busca_cmd = venda_facade.contabilizar_venda_cmd(cursor)
     venda = busca_cmd()
+    corpo = render('vendas/email.txt', {'total': total})
     if venda is None:
         logging.info(total)
+        mail.send_mail(settings.SENDER_EMAIL,
+                       'renzon@gmail.com',
+                       'Contabilização de vendas',
+                       corpo)
     else:
         total = Decimal(total)
         total += venda.preco
--- backend/appengine/settings.py
+++ backend/appengine/settings.py
@@ -12,7 +12,7 @@ from tekton.gae.middleware.webapp2_dependencies import Webapp2Dependencies
 from gaepermission.middleware import LoggedUserMiddleware, PermissionMiddleware
 
 APP_URL = 'https://pythonpro-gae.appspot.com'
-SENDER_EMAIL = 'renzon@gmail.com'
+SENDER_EMAIL = 'renzo@python.pro.br'
 DEFAULT_LOCALE = 'pt_BR'
 DEFAULT_TIMEZONE = 'America/Sao_Paulo'
 LOCALES = ['en_US', 'pt_BR']

c7e003957356c16e9ed5569e0da0216bc787f328
renzon <renzon@gmail.com>
2014-12-29 13:50:19 -0200

    Acessando dados de usuário no Github

---
+++ backend/appengine/routes/github.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from google.appengine.api import urlfetch
+from gaecookie.decorator import no_csrf
+from gaepermission.decorator import login_not_required
+from tekton.router import to_path
+
+
+@login_not_required
+@no_csrf
+def index(_resp, nome):
+    url_base = 'https://api.github.com/users'
+    url_usuario = to_path(url_base, nome)
+    resultado = urlfetch.fetch(url_usuario, headers={'Accept': 'application/vnd.github.v3+json'})
+    _resp.write(resultado.content)

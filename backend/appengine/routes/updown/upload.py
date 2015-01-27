e4802b01bf1cb4544f6e7745fc6a1efd8f5eafa8
renzon <renzon@gmail.com>
2015-01-27 00:42:12 -0200

    Salvamento de blobs com blob_app

--- backend/appengine/routes/updown/upload.py
+++ backend/appengine/routes/updown/upload.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-import logging
-from routes.updown import ok
+from blob_app import blob_facade
+from routes import updown
 from tekton import router
 from tekton.gae.middleware.redirect import RedirectResponse
 
 
-def index(_handler, files):
-    logging.info(files)
+def index(_handler, _logged_user, files):
     blob_infos = _handler.get_uploads('files[]')
-    logging.info(blob_infos)
-    path = router.to_path(ok, blob_key=blob_infos[0].key())
+    cmd = blob_facade.save_blob_files_cmd(blob_infos, _logged_user)
+    cmd.execute()
+    path = router.to_path(updown)
     return RedirectResponse(path)

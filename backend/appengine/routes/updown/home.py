843a54cc3e0af35cd855a1bc40a2c77b0dd321c7
renzon <renzon@gmail.com>
2015-01-27 11:54:28 -0200

    Mostrando dados localizados de arquivos

--- backend/appengine/routes/updown/home.py
+++ backend/appengine/routes/updown/home.py
@@ -22,6 +22,12 @@ def index(_logged_user):
     logging.info(bucket)
     url = blobstore.create_upload_url(success_url, gs_bucket_name=bucket)
     cmd = blob_facade.list_blob_files_cmd(_logged_user)
+    blob_form = blob_facade.blob_file_form()
+
+    def localizar_blob(blob):
+        return blob_form.fill_with_model(blob, 64)
+
+    blob_files = [localizar_blob(b) for b in cmd()]
     context = {'upload_url': url,
-               'blob_files': cmd()}
+               'blob_files': blob_files}
     return TemplateResponse(context, 'updown/home.html')
